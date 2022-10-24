20795c0dbcb0e573b3b4a145b76fc5751cdfd273
Pedro Amaral <amaral1@ufpr.br>
2022-10-24 09:55:04 -0300

    Atualzei lattes.c

--- lattes.c
+++ lattes.c
@@ -1,3 +1,19 @@
+/*
+    Contabilização da produção científica em termos de
+    artigos científicos publicados em periódicos e 
+    conferências de um grupo de pesquisadores por meio
+    da análise de seus currículos Lattes.
+    
+    Desenvolvido por Pedro Amaral Chapelin
+    Data de finalização -> XX/XX/XXXX
+    
+    1 -> lê um arq .xml e imprime seu conteudo --FEITO--
+    2 -> imprimir na tela apenas o nome dos periodicos
+    3 -> imprimir na tela apenas o nome das conferencias
+    4 -> catalogar periodico(s) e imprimi-lo(s) na tela
+    5 -> mesma coisa com conferencia
+    6 -> abre um diretório, le todos os arquivos e imprime seus conteudos 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,7 +21,7 @@ int main ()
 {
     FILE* arq;
     int c;
-
+   
     arq = fopen("curriculoCASTILHO.xml", "r");
 
     // Testa se o arquivo abre
