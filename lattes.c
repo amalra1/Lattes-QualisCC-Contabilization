60aae44fd68f0b8258ed0131d61ba6b536486563
Pedro <amaral1@ufpr.br>
2022-10-23 22:00:52 -0300

    Arquivos preparados

--- lattes.c
+++ lattes.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ()
 {
-    printf("AAAAA\n");
+    FILE* arq;
+    int c;
+
+    arq = fopen("curriculoCASTILHO.xml", "r");
+
+    // Testa se o arquivo abre
+    if (arq == NULL)
+    {
+        printf("Impossivel abrir arquivo\n");
+        exit(1);  // Fecha o programa com status 1
+    }
+
+    c = fgetc(arq);
+    // Pega char a char ate o fim do arquivo
+    while (c != EOF)
+    {
+        printf("%c", c);
+        c = fgetc(arq);
+    }
+
+    fclose(arq);
+    
     return 0;
 }
